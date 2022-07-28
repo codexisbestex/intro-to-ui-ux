88ee5bd1890e1276376982ca7a504d3124aed1a6
Angelique Rivera <angeliqueriveramorales@gmail.com>
2022-07-27 20:35:14 -0400

    installed Vite and Bulma for betterhelp project

---
+++ betterhelp-scaffold/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <section className="hero is-primary is-large">
+      <div className="hero-body">
+        <p className="title">Hero Title</p>
+        <p className="subtitle">Hero Subtitle</p>
+      </div>
+    </section>
+  );
+}
+
+export default App;
