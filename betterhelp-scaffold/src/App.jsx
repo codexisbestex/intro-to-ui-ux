a57d78d6d3084194e72f1220900fc49c2923a106
Angelique Rivera <angeliqueriveramorales@gmail.com>
2022-08-01 22:15:14 -0400

    playing around with Bulma part 1

--- betterhelp-scaffold/src/App.jsx
+++ betterhelp-scaffold/src/App.jsx
@@ -1,10 +1,22 @@
 function App() {
   return (
-    <section className="hero is-primary is-large">
-      <div className="hero-body">
+    <section className="hero is-primary is-large is-outlined">
+      <div className="hero-footer">
         <p className="title">Hero Title</p>
         <p className="subtitle">Hero Subtitle</p>
       </div>
+      <div class="columns">
+  <div class="column">
+    <p class="bd-notification is-info">First column</p>
+    <div class="columns is-mobile">
+      <div class="column">
+        <p class="bd-notification is-info">First nested column</p>
+      </div>
+      <div class="column">
+        <p class="bd-notification is-info">Second nested column</p>
+      </div>
+    </div>
+    </div>
     </section>
   );
 }
