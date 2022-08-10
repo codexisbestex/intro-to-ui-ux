90fa72d043c040e1e4b575a24dcbb0cabfbfc22e
Angelique Rivera <angeliqueriveramorales@gmail.com>
2022-08-10 19:33:18 -0400

    started code on betterhelp hero section

--- betterhelp-scaffold/src/App.jsx
+++ betterhelp-scaffold/src/App.jsx
@@ -1,22 +1,39 @@
 function App() {
   return (
-    <section className="hero is-primary is-large is-outlined">
-      <div className="hero-footer">
-        <p className="title">Hero Title</p>
-        <p className="subtitle">Hero Subtitle</p>
+    <section className="hero is-link is-fullheight">
+      <div className="hero-head">Hero Head</div>
+      <div className="hero-body">
+        <div className="container has-text-centered">
+          <p className="title">You deserve to be happy.</p>
+          <p className="subtitle">What type of therapy are you looking for?</p>
+
+          <div className="columns is-centered">
+            <div className="column is-2">
+              <button className="button is-primary is-fullwidth hero_therapy-type">
+                {" "}
+                Individual <br />
+                (for myself)
+              </button>
+            </div>
+            <div className="column is-2">
+              <button className="button is-primary is-fullwidth">
+                {" "}
+                Couples <br />
+                (for me and my partner)
+              </button>
+            </div>
+            <div className="column is-2">
+              <button className="button is-primary is-fullwidth">
+                {" "}
+                Teen <br />
+                (for my child)
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="columns">
-  <div class="column">
-    <p class="bd-notification is-info">First column</p>
-    <div class="columns is-mobile">
-      <div class="column">
-        <p class="bd-notification is-info">First nested column</p>
-      </div>
-      <div class="column">
-        <p class="bd-notification is-info">Second nested column</p>
-      </div>
-    </div>
-    </div>
+
+      <div className="hero-foot">Hero Foot</div>
     </section>
   );
 }
