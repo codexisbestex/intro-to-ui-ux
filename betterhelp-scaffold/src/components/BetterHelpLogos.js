8fe25c8eb0bb2e51b478f11365bd21d7daf980ae
Angelique Rivera <angeliqueriveramorales@gmail.com>
2022-08-14 12:59:16 -0400

    adding logo component

---
+++ betterhelp-scaffold/src/components/BetterHelpLogos.js
@@ -0,0 +1,16 @@
+export default function BetterHelpLogos(props) {
+  <section>
+    <div className="column is-1">
+      <figure className="image is-5by3"></figure>
+      <img src="../src/assets/company-logos/logo_mha-629462166.jpg"></img>
+      <figure className="image is-5by3"></figure>
+      <img src="../src/assets/company-logos/TMHI_Logo_280x66-Green_Text-Banner.png"></img>
+      <figure className="image is-5by3"></figure>
+      <img src="../src/assets/company-logos/AMSA_logo-300x133.png"></img>
+      <figure className="image is-5by3"></figure>
+      <img src="../src/assets/company-logos/ismho-545457461.png"></img>
+      <figure className="image is-5by3"></figure>
+      <img src="../src/assets/company-logos/Mighty-Logo-800x400.png"></img>
+    </div>
+  </section>;
+}
