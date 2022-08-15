45765df966fe18e664938863d75ca9e6b00b9fc4
Angelique Rivera <angeliqueriveramorales@gmail.com>
2022-08-15 18:42:30 -0400

    adding Hero and Logos components to App

--- betterhelp-scaffold/src/App.jsx
+++ betterhelp-scaffold/src/App.jsx
@@ -1,41 +1,12 @@
-function App() {
+import Hero from "./components/hero/Hero.jsx";
+import Logos from "./components/Logos.jsx";
+
+export default function App() {
   return (
-    <section className="hero is-link is-fullheight">
-      <div className="hero-head">Hero Head</div>
-      <div className="hero-body">
-        <div className="container has-text-centered">
-          <p className="title">You deserve to be happy.</p>
-          <p className="subtitle">What type of therapy are you looking for?</p>
-
-          <div className="columns is-centered">
-            <div className="column is-2">
-              <button className="button is-primary is-fullwidth hero_therapy-type">
-                {" "}
-                Individual <br />
-                (for myself)
-              </button>
-            </div>
-            <div className="column is-2">
-              <button className="button is-primary is-fullwidth">
-                {" "}
-                Couples <br />
-                (for me and my partner)
-              </button>
-            </div>
-            <div className="column is-2">
-              <button className="button is-primary is-fullwidth">
-                {" "}
-                Teen <br />
-                (for my child)
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="hero-foot">Hero Foot</div>
-    </section>
+    <main>
+      <Hero />
+      <Logos />
+      <p>Show everything please</p>
+    </main>
   );
 }
-
-export default App;
---
+++ betterhelp-scaffold/src/components/hero/Hero.jsx
@@ -0,0 +1,37 @@
+export default function Hero() {
+  <section>
+    <div className="hero-head">Hero Head</div>
+    <div className="hero-body">
+      <div className="container has-text-centered">
+        <p className="title">You deserve to be happy.</p>
+        <p className="subtitle">What type of therapy are you looking for?</p>
+
+        <div className="columns is-centered">
+          <div className="column is-2">
+            <button className="button is-primary is-fullwidth hero_therapy-type">
+              {" "}
+              Individual <br />
+              (for myself)
+            </button>
+          </div>
+          <div className="column is-2">
+            <button className="button is-primary is-fullwidth">
+              {" "}
+              Couples <br />
+              (for me and my partner)
+            </button>
+          </div>
+          <div className="column is-2">
+            <button className="button is-primary is-fullwidth">
+              {" "}
+              Teen <br />
+              (for my child)
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div className="hero-foot">Hero Foot</div>
+  </section>;
+}
